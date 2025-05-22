20afc2cca2902a87aa68ff8be8c9d9b48026ee5b
ZarRomM <86100277+ZarRomM@users.noreply.github.com>
2025-05-22 10:01:14 -0600

    git commit -m "Starting base files"

---
+++ Proyect_7_Streamlit.py
@@ -0,0 +1,12 @@
+# Proyect_7_using_Streamlit_platform
+
+# Importing libraries
+import pandas as pd
+import numpy as np
+import streamlit as st
+import matplotlib.pyplot as plt
+import plotly.express as px
+import seaborn as sns
+
+print('Starting the app...')
+# Set the title of the app
